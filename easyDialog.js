a9e87be8d2aebe15284dc0b7b697551c11e877a8
xiaomingming <lwm8817@126.com>
2014-03-13 10:17:36 +0800

    增加浮层和遮罩层叠值关系处理；增加是否显示底部接口；取消对确定按钮的自动关闭

--- easyDialog.js
+++ easyDialog.js
@@ -1,19 +1,6 @@
 /*
  * author:leweiming
- * gmail:xmlovecss 艾特 gmail dot com
- * 创建一个简单的dialog
- * 自定义弹层包含块（这样可以有效地设计不同的弹层样式），自定义内容宽高，自定义位置（比如上左，上右，下左，下右）
- * 弹层可以定义在多少秒后自动关闭
- * 设置内容宽高，否则，将使用系统默认宽高
- * 自定义填充内容
- * 自定义关闭回调
- * 支持内容异步获取（这个为嘛好难）
- * 异步阻塞会产生一个问题，未响应时，内容区为空，没有高度；响应后，还得重新计算高度
- * example:
- *       $('.easy-dialog').easyDialog({
- *           'dTitle':'lallalala',
- *           'dContentTmp':'<p>ixixixixixixixixii</p>'
- *       });
+ * gmail:xmlovecss # gmail dot com
  */
 (function(window, $, undefined) {
     var my = {},
@@ -29,6 +16,12 @@
     my.isIE6 = function() {
         return $.browser.msie && ($.browser.version === '6.0');
     };
+    // 启动渲染模板
+    my.renderTmp = function(wrapper, cont) {
+        // 先清空，再填充，避免产生重复填充的问题
+        wrapper.html('').append(cont);
+    };
+
     // 构造函数开始
     my[constructorFunName] = function(container, options) {
         var settings = $.extend({}, $.fn[pluginName].defaults, options);
@@ -42,34 +35,47 @@
         this.dTitle = settings.dTitle;
         // 弹层关闭内容
         this.dCloseTxt = settings.dCloseTxt;
+        // 是否关闭弹层后，销毁弹层
+        this.isDestroy = settings.isDestroy;
         this.dialogFilterTmp = '<div class="easy-dailog-filter"></div>';
+        // 是否显示页脚
+        this.isShowFooter = settings.isShowFooter;
         this.dialogTmp = [
             '<div class="easy-dialog-header">',
-            '<h1>'+this.dTitle+'</h1>',
-            '<a href="#" class="easy-dialog-close">'+this.dCloseTxt+'</a>',
+            '<h1>' + this.dTitle + '</h1>',
+            '<a href="javascript:void(0)" class="easy-dialog-close">' + this.dCloseTxt + '</a>',
             '</div>',
             '<div class="easy-dialog-content">',
             '<div class="easy-dialog-content-inner"></div>',
             '</div>',
             '<div class="easy-dialog-footer">',
             '<p>',
-            '<button type="'+this.dOKType+'" class="btn-confirm">'+this.dOKVal+'</button>',
+            '<button type="' + this.dOKType + '" class="btn-confirm">' + this.dOKVal + '</button>',
             '</p>',
             '</div>'
         ].join('');
         // 模板渲染
         container.html('').append(this.dialogTmp);
+        // my.renderTmp(this.container);
         // 头部
         this.header = container.find('.easy-dialog-header');
         // 内容区域
         this.cont = container.find('.easy-dialog-content .easy-dialog-content-inner');
         // 底部
         this.footer = container.find('.easy-dialog-footer');
+        // 是否移除页脚
+        if (!this.isShowFooter) {
+            /*修复jquery 在firefox下的 dom remove 方法bug，移除后的dom css定义高度仍然会被计算*/
+            this.footer.remove().css('height','0');
+        }
+
         // 弹层层叠值
         this.zindex = settings.zindex;
+
         // 弹层填充内容
         // 目前仅仅支持html片段
         this.dContentTmp = settings.dContentTmp;
+
         // 弹层内容宽度设置
         // 此时还不是精确宽高
         // 需要等到内容填充完成后获取
@@ -83,7 +89,7 @@
         this.pBottom = settings.pBottom;
         // 关闭
         this.closeBtn = this.header.find('.easy-dialog-close');
-        
+
         // 确定，取消按钮
         this.confirmBtn = this.footer.find('.btn-confirm');
         // this.cancelBtn = this.footer.find('.btn-cancel');
@@ -100,8 +106,8 @@
         init: function() {
             this
                 .renderDialogContent()
-                // .renderDialogTitle()
-                // .renderDialogFooter()
+                .renderDialogTitle()
+                .renderDialogFooter()
                 .renderDialogModel()
                 .eventControl();
             this.isShowFilter && this.renderFilterContent();
@@ -115,13 +121,10 @@
             this.cWidth = this.cWidth || this.cont.outerWidth();
             // 弹层内容高度
             this.cHeight = this.cHeight || this.cont.outerHeight();
-
             var headerHeight = this.header.outerHeight(),
                 footerHeight = this.footer.outerHeight(),
                 containerHeight = this.cHeight + headerHeight + footerHeight;
-
-            // console.log(_this.cHeight, headerHeight, footerHeight);
-
+            
             return {
                 headerHeight: headerHeight,
                 footerHeight: footerHeight,
@@ -185,6 +188,21 @@
             this.container.css(dialogStyleSettings).show();
             return this;
         },
+        // 盒模型确定好后，
+        // 开始进行内容渲染
+        // 渲染头，内容，底部
+        setDialogTitle: function() {
+            return this.dTitle;
+        },
+        renderDialogTitle: function() {
+            this.header.find('h1').text(this.setDialogTitle());
+            this.header.find('.easy-dialog-close').text(this.dCloseTxt);
+            return this;
+        },
+        renderDialogFooter: function() {
+            this.confirmBtn.text(this.dOKVal);
+            return this;
+        },
         setDialogContent: function(cont) {
             if (my.isYourType(this.dContentTmp, 'function')) {
                 return this.dContentTmp(cont);
@@ -198,7 +216,6 @@
         // 关闭不受此影响
         renderDialogContent: function() {
             var _this = this;
-
             // 判断设置内容回调
             // 若是返回串，则插入到this.cont
             // 否则传入this.cont，进行回调
@@ -210,18 +227,20 @@
 
             // 确定，取消
             this.confirmBtn.on('click', $.proxy(_this.confirmEvent, _this));
-            // this.cancelBtn.on('click', $.proxy(_this.cancelEvent, _this));
             return this;
         },
         // 渲染遮罩层
         renderFilterContent: function() {
+            var _this = this;
             // 若遮罩层存在，则不进行重复插入
             !($('.easy-dailog-filter').length) && $('body').append(this.dialogFilterTmp);
             // 为了兼容IE6，设置遮罩层高度
             // 高度为浮层高度和document高度中最大的
             var filterHeight = Math.max(this.getDialogModelStyle().containerHeight, $(document).height());
-            // console.log(this.container);
-            $('.easy-dailog-filter').css('height', filterHeight + 'px').show();
+            $('.easy-dailog-filter').css({
+                'height': filterHeight + 'px',
+                'z-index': _this.zindex - 1
+            }).show();
             return this;
         },
         // 隐藏弹层
@@ -262,20 +281,19 @@
         // 按钮关闭
         // 这里要考虑，关闭按钮后，是否清空内容区，还是仅仅隐藏
         closeEvent: function() {
-            // console.log('close filter');
-            this.hideDialog();
-            // 若有弹层，关闭弹层
+            if (this.isDestroy) {
+                this.container.html('').attr('style', '').removeData('plugin-' + pluginName);
+            } else {
+                this.hideDialog();
+            }
+            // 遮罩层考虑不进行清空
             this.hideFilter();
         },
         // 确定事件
-        // 始终会调用默认关闭
-        // 是应当先关闭，再执行回调
-        // 还是应当先执行完回调，再关闭呢
+        // 只有回调函数返回true，才执行关闭
         confirmEvent: function() {
             // 此处对于是否存在回调，要做进一步的判断
-            this.closeEvent();
-            my.isYourType(this.OK, 'function') && this.OK();
-
+            my.isYourType(this.OK, 'function') && this.OK() && this.closeEvent();
         },
         // 取消事件
         cancelEvent: function() {
@@ -309,15 +327,19 @@
      */
     $.fn[pluginName].defaults = {
         cWidth: 300,
-        dOKType:'button',
-        dOKVal:'确定',
-        dTitle:'',
-        dCloseTxt:'关闭',
+        dOKType: 'button',
+        dOKVal: '确定',
+        dTitle: '',
+        dCloseTxt: '关闭',
+        isDestroy: false,
+        isShowFooter: true,
         isShowFilter: true,
         dContentTmp: function() {
             return '';
         },
-        zindex: 4,
-        OK: function() {}
+        zindex: 11,
+        OK: function() {
+            return false; //点击确定，需要自动关闭，返回true即可
+        }
     };
 })(window, jQuery);
