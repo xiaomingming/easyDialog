177bb0ae83b55359ddb63beae589c84e50fe0e9c
leweiming <leweiming@51talk.com>
2018-09-20 19:28:59 +0800

    更新

--- auto_build.sh
+++
@@ -1,9 +0,0 @@
-#! /bin/bash
-# SITE_PATH='https://github.com/xiaomingming/easyDialog'
-# USER='admin'
-# USERGROUP='admin'
-# cd $SITE_PATH
-git pull origin/dev_dialog_v1
-git push origin master
-git checkout master
-# chown -R $USER:$USERGROUP $SITE_PATH
